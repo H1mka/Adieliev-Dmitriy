e58e018f99622f389678641b4af5bfe86041f0bd
Adelev <kak-dela1488@ukr.net>
2023-09-19 18:07:52 +0300

    delete server

--- server/routes/ideasList.routes.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const ideasListController = require('../controllers/ideasList.controller');
-
-router.get('/getIdeasList', ideasListController.getIdeasList);
-router.post('/postIdeasList', ideasListController.postIdeasList);
-
-module.exports = router;
