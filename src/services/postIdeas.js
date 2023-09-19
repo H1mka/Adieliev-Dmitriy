5f37d31f1b645738405c07d44fbba26119e03b4d
Adelev <kak-dela1488@ukr.net>
2023-09-19 18:37:05 +0300

    delete unnecessary packages

--- src/services/postIdeas.js
+++ src/services/postIdeas.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const postIdeas = (link, data) => {
     axios
-        .post(`http://localhost:3001/${link}`, data)
+        .post(`http://localhost:5001/${link}`, data)
         .then((response) => {
             console.log('Data uploaded successfully');
         })
